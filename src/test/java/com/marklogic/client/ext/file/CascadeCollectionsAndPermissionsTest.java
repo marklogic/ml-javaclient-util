7a80565e3e19498f90d311dc82d4d0257bf7abda
Rob Rudin <rob.rudin@progress.com>
2023-09-06 17:16:24 -0400

    Reverting the change for configuring cascading

--- src/test/java/com/marklogic/client/ext/file/CascadeCollectionsAndPermissionsTest.java
+++ src/test/java/com/marklogic/client/ext/file/CascadeCollectionsAndPermissionsTest.java
@@ -17,13 +17,25 @@ package com.marklogic.client.ext.file;
 
 import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.ext.AbstractIntegrationTest;
+import com.marklogic.client.ext.modulesloader.impl.AssetFileLoader;
+import com.marklogic.client.ext.modulesloader.impl.DefaultModulesFinder;
+import com.marklogic.client.ext.modulesloader.impl.DefaultModulesLoader;
+import com.marklogic.client.io.DocumentMetadataHandle;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.Resource;
+
+import java.nio.file.Paths;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CascadeCollectionsAndPermissionsTest extends AbstractIntegrationTest {
 
 	final private String PARENT_COLLECTION = "ParentCollection";
 	final private String CHILD_COLLECTION = "ChildCollection";
+
+	private GenericFileLoader loader;
 
 	@BeforeEach
 	public void setup() {
@@ -38,20 +50,23 @@ public class CascadeCollectionsAndPermissionsTest extends AbstractIntegrationTes
 		GenericFileLoader loader = new GenericFileLoader(client);
 		loader.loadFiles(directory);
 
-		verifyCollections( "/child1_1-noCP/test.json", PARENT_COLLECTION);
-		verifyPermissions( "/child1_1-noCP/test.json", "rest-writer", "update");
+		verifyCollections("/child1/child1.json", "ParentCollection");
+		verifyPermissions("/child1/child1.json", "rest-writer", "update");
+
+		verifyCollections("/child1_1-noCP/test.json", PARENT_COLLECTION);
+		verifyPermissions("/child1_1-noCP/test.json", "rest-writer", "update");
 
 		verifyCollections( "/child1_2-withCP/test.json", CHILD_COLLECTION);
 		verifyPermissions( "/child1_2-withCP/test.json", "rest-reader", "read");
 
-		verifyCollections( "/child3_1-withCP/grandchild3_1_1-noCP/test.json", CHILD_COLLECTION);
-		verifyPermissions( "/child3_1-withCP/grandchild3_1_1-noCP/test.json", "rest-reader", "read");
-
-		verifyCollections("/child1/child1.json", "ParentCollection");
-		verifyPermissions("/child1/child1.json", "rest-writer", "update");
-
 		verifyCollections("/child2/child2.json", "child2");
 		verifyPermissions("/child2/child2.json", "app-user", "read");
+
+		verifyCollections("/child3_1-withCP/test.json", CHILD_COLLECTION);
+		verifyPermissions("/child3_1-withCP/test.json", "rest-reader", "read");
+
+		verifyCollections("/child3_1-withCP/grandchild3_1_1-noCP/test.json", CHILD_COLLECTION);
+		verifyPermissions("/child3_1-withCP/grandchild3_1_1-noCP/test.json", "rest-reader", "read");
 
 		verifyCollections("/parent.json", "ParentCollection");
 		verifyPermissions("/parent.json", "rest-writer", "update");
@@ -74,5 +89,47 @@ public class CascadeCollectionsAndPermissionsTest extends AbstractIntegrationTes
 
 		verifyCollections( "/child2_3-withCnoP/grandchild2_3_1-withPnoC/test.json", PARENT_COLLECTION);
 		verifyPermissions( "/child2_3-withCnoP/grandchild2_3_1-withPnoC/test.json", "rest-reader", "read");
+	}
+
+	/**
+	 * Verifies that by default, cascading is disabled. This is to preserve backwards compatibility in 4.x. We
+	 * expect to change this for 5.0.
+	 */
+//	@Test
+//	void cascadingDisabled() {
+//		loader = new GenericFileLoader(client);
+//
+//		loader.loadFiles("src/test/resources/process-files/cascading-metadata-test/parent1-withCP");
+//		verifyCollections("/child1_1-noCP/test.json");
+//		verifyPermissions("/child1_1-noCP/test.json");
+//	}
+
+	@Test
+	public void withPropertiesFiles() {
+		AssetFileLoader fileLoader = new AssetFileLoader(client);
+		fileLoader.setPermissions("rest-extension-user,read,rest-extension-user,update,rest-extension-user,execute");
+
+		DefaultModulesLoader modulesLoader = new DefaultModulesLoader(fileLoader);
+		modulesLoader.setModulesManager(null);
+
+		String dir = Paths.get("src", "test", "resources", "base-dir-with-properties-files").toString();
+		Set<Resource> files = modulesLoader.loadModules(dir, new DefaultModulesFinder(), client);
+		assertEquals(2, files.size());
+
+		DocumentMetadataHandle metadata = new DocumentMetadataHandle();
+
+		client.newDocumentManager().readMetadata("/root.sjs", metadata);
+		assertEquals(1, metadata.getCollections().size());
+		assertEquals("parent", metadata.getCollections().iterator().next());
+		DocumentMetadataHandle.DocumentPermissions perms = metadata.getPermissions();
+		assertEquals(DocumentMetadataHandle.Capability.READ, perms.get("qconsole-user").iterator().next());
+		assertEquals(3, perms.get("rest-extension-user").size());
+
+		client.newDocumentManager().readMetadata("/lib/lib.sjs", metadata);
+		assertEquals(1, metadata.getCollections().size());
+		assertEquals("lib", metadata.getCollections().iterator().next());
+		perms = metadata.getPermissions();
+		assertEquals(DocumentMetadataHandle.Capability.UPDATE, perms.get("app-user").iterator().next());
+		assertEquals(3, perms.get("rest-extension-user").size());
 	}
 }
